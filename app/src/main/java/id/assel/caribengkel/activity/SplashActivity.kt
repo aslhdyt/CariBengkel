95cbcb5798e7de17548f0f2fa8529f1e44ccd866
Assel <aslhdyt@gmail.com>
2018-05-25 20:42:34 +0700

    initial login

--- app/src/main/java/id/assel/caribengkel/activity/SplashActivity.kt
+++ app/src/main/java/id/assel/caribengkel/activity/SplashActivity.kt
@@ -3,19 +3,22 @@ package id.assel.caribengkel.activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import id.assel.caribengkel.R
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val auth = FirebaseAuth.getInstance()
+
         //todo check is logged in or  not
-        if (true){
+        if (auth.currentUser != null){
             val i = Intent(this, MapsActivity::class.java)
             startActivity(i)
         } else {
             val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
         }
 
     }
